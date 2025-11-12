757bcce883964e83548ac7be499d2c66e194e3a4
Rebeca Almeida Ott <rebecaaott@gmail.com>
2025-11-12 09:24:27 -0300

    resolvido

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,39 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-import {Container} from "./components/Container"
+import { Container } from "./components/Container"
 import { Title } from './components/Title'
-import {Table} from "./components/Table"
-import {movies} from "./components/Movies"
+import { Table } from "./components/Table"
+import { movies } from "./components/Movies"
 
 function App() {
   const [search, setSearch] = useState('');
 
-  const filteredMovies = movies.filter((movie)=> movie.nome.includes(search));
+  const filteredMovies = movies.filter((movie) =>
+    movie.nome.toLowerCase().includes(search.toLowerCase())
+  );
+
+  useEffect(() => {
+    console.log("dispara o search toda vez q muda")
+  }, [search])
+
+  useEffect(() => {
+    console.log("executa somente na primeira renderizacao")
+  }, [])
+
+  useEffect(() => {
+    console.log("executa toda hora")
+  })
 
   return (
     <div className="App">
       <header className="App-header">
 
         <Container children={<Title />} />
-        
+
         <input type="text"
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}/>
-        
+          value={search}
+          onChange={(e) => setSearch(e.target.value)} />
+
         <Container>
           <Table movies={filteredMovies} />
         </Container>
